833ecba8aeaea8877f82ffea62bbc30e150071aa
Kartik Annasaheb Kesbhat <101994374+kartikkesbhat-2003@users.noreply.github.com>
2024-01-14 15:18:46 +0530

    Add files via upload

---
+++ User Profile Card/index.js
@@ -0,0 +1,10 @@
+
+const modal = document.querySelector('.model-container')
+
+function openModel() {
+    modal.classList.remove('hide-model');
+}
+
+function closeModel() {
+    modal.classList.add('hide-model');
+}
